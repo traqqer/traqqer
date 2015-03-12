5436b36f43e15f0fcb69e75b8c0f7f7be4882dbc
John Boggs <jboggs@Johns-MacBook-Pro.local>
2015-03-11 20:48:25 -0700

    Add Storyboard files and linked classes for each ViewController

---
+++ Traqqer/DetailsStatsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailsStatsViewController.swift
+//  Traqqer
+//
+//  Created by John Boggs on 3/11/15.
+//  Copyright (c) 2015 John Boggs. All rights reserved.
+//
+
+import UIKit
+
+class DetailsStatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
