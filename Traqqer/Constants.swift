bd6e67222327e9d7087224be4b4fcb9168f00755
John Boggs <jboggs@Johns-MacBook-Pro.local>
2015-03-11 21:31:01 -0700

    Wiring up views

---
+++ Traqqer/Constants.swift
@@ -0,0 +1,34 @@
+//
+//  Constants.swift
+//  Traqqer
+//
+//  Created by John Boggs on 3/11/15.
+//  Copyright (c) 2015 John Boggs. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct StoryboardIdentifiers {
+        static let DASHBOARD = "Dashboard"
+        static let DASHBOARD_STATS = "DashboardStats"
+        static let DASHBOARD_GRAPHS = "DashboardGraphs"
+        static let DETAILS = "Details" // No Custom Controlller
+        static let DETAILS_STATS = "DetailsStats"
+        static let DETAILS_GRAPHS = "DetailsGraphs"
+        static let NEW_STAT = "NewStat"
+        static let NEW_STAT_FORM = "NewStatForm"
+    }
+    struct ViewControllerIdentifiers {
+        static let DASHBOARD = "Dashboard"
+        static let DASHBOARD_STATS = "DashboardStats"
+        static let DASHBOARD_GRAPHS = "DashboardGraphs"
+        static let DETAILS_STATS = "DetailsStats"
+        static let DETAILS_GRAPHS = "DetailsGraphs"
+        static let NEW_STAT = "NewStat"
+        static let NEW_STAT_FORM = "NewStatForm"
+    }
+    struct ReuseIdentifiers {
+        
+    }
+}
--- Traqqer/DashboardViewController.swift
+++ Traqqer/DashboardViewController.swift
@@ -9,27 +9,35 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
-
+    var tabVC : UITabBarController!
+//    var viewControllers : NSArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupTabViewController()
     }
     
+    func setupTabViewController() {
+        let dashboardStatsVC = UIStoryboard(
+            name: Constants.StoryboardIdentifiers.DASHBOARD_STATS,
+            bundle: nil
+        ).instantiateViewControllerWithIdentifier(
+            Constants.ViewControllerIdentifiers.DASHBOARD_STATS
+        ) as DashboardStatsViewController
 
-    /*
-    // MARK: - Navigation
+        let dashboardGraphsVC = UIStoryboard(
+            name: Constants.StoryboardIdentifiers.DASHBOARD_GRAPHS, bundle: nil
+        ).instantiateViewControllerWithIdentifier(
+            Constants.ViewControllerIdentifiers.DASHBOARD_GRAPHS
+        ) as DashboardGraphsViewController
+        
+        dashboardStatsVC.tabBarItem!.title = "Stats"
+        dashboardGraphsVC.tabBarItem!.title = "Graphs"
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        tabVC = UITabBarController()
+        
+        tabVC.viewControllers = [dashboardStatsVC, dashboardGraphsVC]
+        self.view.addSubview(tabVC.view)
+        
     }
-    */
-
 }
