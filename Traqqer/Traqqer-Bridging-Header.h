71c6d2ea0090c54e41020cf4359e9943ed45f63a
steffanc <steffan.chartrand@gmail.com>
2015-03-14 10:56:53 -0700

    adding in jbchartview to podfile and dependencies in bridging header

---
+++ Traqqer/Traqqer-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <AFNetworking/AFNetworking.h>
+#import "UIImageView+AFNetworking.h"
+#import <UIKit/UIKit.h>
+#import "JBChartView.h"
+#import "JBBarChartView.h"
+#import "JBLineChartView.h"
